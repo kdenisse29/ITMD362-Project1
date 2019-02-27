ee916330c559fd5f05c0e340e6631d4550f6db2c
Kelly <kdenisse29@gmail.com>
2019-02-27 17:40:13 -0600

    Erased telephone sections

--- site.js
+++ site.js
@@ -1,5 +1,4 @@
 var name_input = document.querySelector('#name');
-var tel_input = document.querySelector('#telephone');
 var email_input = document.querySelector('#email');
 var submit_data = document.querySelector('#submit');
 (function() {
@@ -9,25 +8,6 @@ var submit_data = document.querySelector('#submit');
   }
 document.addEventListener('DOMContentLoaded',function();
 submit_data.setAttribute('disabled','disabled');
-// Data cleanup functions
-  function clean_nonnumbers(value) {
-    // returns value with all non-digits removed
-    return value.replace(/\D/g,'');
-  }
-  function clean_whitespace(value) {
-    // returns value with all whitespace characters removed
-    return value.replace(/\s/g, '');
-  }
-
-// Phone-specific santizier functions
-  function strip_us_country_code(value) {
-    return value.replace(/^1/,'');
-  }
-// Phone validity functions
-  function validate_us_phone(value) {
-    var phone_number = strip_us_country_code(clean_nonnumbers(value));
-    return validate(phone_number.length,eq,10);
-  }
 
 // Email validity function
   function validate_email(value) {
@@ -38,7 +18,7 @@ submit_data.setAttribute('disabled','disabled');
 // Listen for keyup event ANYWHERE in the form
   signup_form.addEventListener('keyup',function(){
 // Check the likely validity of phone AND email
-  if (validate_us_phone(tel_input.value) && validate_email(email_input.value)) {
+  if (validate_email(email_input.value) {
 // If both are valid, remove the disabled attribute on the submit button
     signup_submit.removeAttribute('disabled');
   }
