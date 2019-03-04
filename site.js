8c34f2276f07d928a3c8964ac36331e41306202c
kdenisse29 <kdenisse29@gmail.com>
2019-03-04 05:18:34 -0600

    S validator fixes

--- site.js
+++ site.js
@@ -1,28 +1,53 @@
-var name_input = document.querySelector('#name');
-var email_input = document.querySelector('#email');
-var submit_data = document.querySelector('#submit');
 (function() {
   if (!('querySelector' in document && 'addEventListener' in document)) {
     console.log('Old browser');
     return;
   }
-document.addEventListener('DOMContentLoaded',function();
-submit_data.setAttribute('disabled','disabled');
-
-// Email validity function
+  // Library of comparison functions
+  //
+  // Unlike the raw operators these encapsulate, functions
+  // can be passed around like any other value into other
+  // functions.
+  function eq(value,condition) {
+    return value === condition;
+  }
+  
+  function validate(value,check,condition) {
+    if (eq(typeof(check.test),'function')) {
+      // Handle a regular expression
+      return check.test(value);
+    } else if (eq(typeof(check),'function')) {
+      // Handle a comparison function
+      return check(value,condition);
+    } else {
+      return false;
+    }
+  }
+  // Data cleanup functions
+  function clean_whitespace(value) {
+    // returns value with all whitespace characters removed
+    return value.replace(/\s/g, '');
+  }
+  // Email validity function
   function validate_email(value) {
     var email = clean_whitespace(value);
     return validate(email,/^[^@\s]+@[^@\s]+$/g);
   }
-  
+
+  document.addEventListener('DOMContentLoaded',function(){
+  var signup_form = document.querySelector('#mailinglist');
+  var name_input = document.querySelector('#name');
+  var submit_data = document.querySelector('#submit');
+  var email_input = document.querySelector('#email');
 // Listen for keyup event ANYWHERE in the form
   signup_form.addEventListener('keyup',function(){
-// Check the likely validity of phone AND email
-  if (validate_email(email_input.value) {
+// Check the likely validity of email
+  if (validate_email(email_input.value)){
 // If both are valid, remove the disabled attribute on the submit button
-    signup_submit.removeAttribute('disabled');
-  }
-  else {
+    submit_data.removeAttribute('disabled');
+  } else {
 // This will re-disable the submit button if the input changes to an invalid state
-    signup_submit.setAttribute('disabled','disabled');
-  }
+    submit_data.setAttribute('disabled','disabled');
+  }});
+ });
+}());
